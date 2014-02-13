0176634e595b86277e574f67dfb96445dcac593b
ArturVasilov <artur_8005@mail.ru>
2014-02-14 00:02:07 +0400

    Created baseline of project
    Math expressions: we get some string, that is some math expr with scobs,
    sums, production, dividing

---
+++ MathExpressions/MathExpressions/Expression.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExpressions
+{
+    class Expression
+    {
+        private String expr;
+
+        public Expression(String expr)
+        {
+            if (rightString(expr))
+                if (StackScobs.checkScobs(expr))
+                    this.expr = expr;
+                else throw new Exception("Illegal scobs in expression!!");
+            else throw new Exception("Illegal Expression!!");
+        }
+
+
+        private static class StackScobs
+        {
+            String str;
+
+            public String Str
+            {
+                get { return str; }
+                set { str = value; }
+            }
+
+            public StackScobs(String str)
+            {
+                Str = str;
+            }
+
+            public static bool checkScobs(String checkString) 
+            {
+                return true;
+            }
+        }
+
+
+        public double exprResult()
+        {
+            return 0;
+        }
+
+        private bool rightString(String checkString)
+        {
+            return true;
+        }
+    }
+
+}
---
+++ MathExpressions/MathExpressions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MathExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String expression = Console.ReadLine();
+        }
+
+        private void test()
+        {
+            StreamReader sw = new StreamReader("input.txt");
+            int n = 0;
+            #region getN
+            try
+            {
+                n = Convert.ToInt32(sw.ReadLine());
+            }
+            catch (FormatException)
+            {
+                throw new Exception("Invalid n!");
+            }
+            catch (Exception)
+            {
+                throw new Exception("Something strange with n!");
+            }
+            #endregion
+
+            StreamWriter sr = new StreamWriter("input.txt");
+            String str;
+            for (int i = 0; i < n; i++)
+            {
+                str = sw.ReadLine();
+                Expression expr = new Expression(str);
+                sr.WriteLine(expr.exprResult());
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
